c718a655f99f619146a8a0fe868b46c27ab35386
dennis910130 <sic046@ucsd.edu>
2014-04-13 12:00:33 -0700

    modified:   AWSCredentials.py

--- LocalScripts/AWSCredentials.py
+++ LocalScripts/AWSCredentials.py
@@ -7,4 +7,4 @@ aws_secret_access_key='zE6ZGGs7oIzszKHlTjoIVT4nGOrvQ5Uuo8n92IJP'
 # Get the Keypair in the EC2 Dashboard page.
 keyPairFile="</Users/chensi/.ssh/CSE291.pem>" # name of file keeping local key
 key_name="CSE291" # name of keypair (not name of file where key is stored)
-
+security_groups=['default']
